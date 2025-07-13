046af4f1e647ad56123de7755ff8a9edf581cd26
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-07-13 17:01:54 +0530

    Fixes

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -13,27 +13,21 @@ const Header = () => {
         />
       </div>
 
-     <nav className="flex gap-6">
-  {["EXPERIENCE", "PROJECTS"].map((item) => {
-    const id = item.toLowerCase();
-    return (
-      <Button
-  key={item}
-  name={item}
-  onClick={() => {
-    const el = document.getElementById(item.toLowerCase());
-    if (el) {
-      el.scrollIntoView({ behavior: "smooth" });
-    }
-  }}
-  className="relative text-white cursor-pointer hover:before:w-full before:w-0 before:h-[2px] before:absolute before:bottom-0 before:left-0 before:bg-white before:transition-all before:duration-300"
-/>
-
-    );
-  })}
-</nav>
-
-
+      <nav className="flex gap-6">
+        {["EXPERIENCE", "PROJECTS"].map((item) => (
+          <Button
+            key={item}
+            name={item}
+            onClick={() => {
+              const el = document.getElementById(item.toLowerCase());
+              if (el) {
+                el.scrollIntoView({ behavior: "smooth" });
+              }
+            }}
+            className="relative text-white cursor-pointer hover:before:w-full before:w-0 before:h-[2px] before:absolute before:bottom-0 before:left-0 before:bg-white before:transition-all before:duration-300"
+          />
+        ))}
+      </nav>
 
       <div className="flex items-center">
         <InteractiveHoverButton className="m-2" />
